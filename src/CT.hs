4a22d7253af91b6b711b4b8b8b5bd4d9cbc39af0
Tim Holzschuh <timholzschuh@gmx.de>
2017-04-14 13:50:05 +0200

    still far from perfect

--- src/CT.hs
+++ src/CT.hs
@@ -24,12 +24,15 @@ vertexRange :: Dim -> Int
 vertexRange (dimX, dimY, dimZ) = dimX * dimY * dimZ - 1
 
 coord2vert :: Dim -> Coord -> Vertex
-coord2vert (dimX, dimY, dimZ) (x,y,z) = case (x,y,z) of
-  (0,0,0) -> 0
-  (x,0,z) -> z * dimX * dimY + x
-  (x,y,0) -> y * dimX + x
-  (0,y,z) -> z * dimX * dimY + y * dimX
-  (x,y,z) -> z * dimX * dimY + y * dimX + x
+coord2vert (dimX, dimY, dimZ) (x,y,z) = if (x >= dimX || y >= dimY || z >= dimZ)
+  then -1
+  else
+    case (x,y,z) of
+      (0,0,0) -> 0
+      (x,0,z) -> z * dimX * dimY + x
+      (x,y,0) -> y * dimX + x
+      (0,y,z) -> z * dimX * dimY + y * dimX
+      (x,y,z) -> z * dimX * dimY + y * dimX + x
 
 vert2coord :: Dim -> Vertex -> Coord
 vert2coord dim@(dimX, dimY, dimZ) vert
@@ -46,7 +49,8 @@ vert2coord dim@(dimX, dimY, dimZ) vert
 dataset :: Dim -> (Coord -> [Coord]) -> [sc] -> Dataset sc
 dataset dim nbhood samples = Dataset dim (neighbours dim nbhood) ((A.!)array)
     where
-      array                    = A.listArray (0, vertexRange dim) samples
+      array = A.listArray (0, vertexRange dim) samples
+
       neighbours :: Dim -> (Coord -> [Coord]) -> (Vertex -> [Vertex])
       neighbours dim nbhood vertex = Prelude.map (coord2vert dim) (nbhood $ vert2coord dim vertex)
 
@@ -54,7 +58,7 @@ dataset dim nbhood samples = Dataset dim (neighbours dim nbhood) ((A.!)array)
 neighbourhood :: Dim -> Coord -> [Coord]
 neighbourhood (dimX, dimY, dimZ) coord@(x,y,z) = Prelude.filter edgeCase [ (x+i, y+j, z+k) | i <- [-1,0,1], j <- [-1,0,1], k <- [-1,0,1] ]
     where
-      edgeCase = \v@(x',y',z') -> (x' >= 0 && y' >= 0 && z' >= 0) && coord /= v
+      edgeCase = \v@(x',y',z') -> coord /= v && (x' >= 0 && y' >= 0 && z' >= 0 && x' < dimX && y' < dimY && z' < dimZ)
 
 
 type Node = Int
@@ -94,16 +98,17 @@ joinTree vertices adjacent = tree $ foreach vertices (initJoin vertices) $ \jI v
       let components   = disj jI
           lowest       = base jI
           comp s       = case I.lookup s components of
-                         (Just c) -> c
-                         Nothing  -> S.empty
+                           (Just c)   -> c
+                           (Nothing)  -> S.empty
           compV        = comp v
           compN        = comp n
-          replaceUnion = \_ -> S.union compV compN
+          union        = S.union compV compN
+          replaceUnion = \_ -> union
           u            = lowest compN
       in if ( n `S.member` (seen jI) ) && (compV /= compN)
          then
                jI {
-                  disj = I.adjust replaceUnion v components,
+                  disj = I.adjust replaceUnion n components,
                   tree = (u,v) : (tree jI),
                   base = set (set lowest compV v) compN v,
                   seen = S.insert n (seen jI)
@@ -117,11 +122,21 @@ contourTree :: Graph -> Graph -> EdgeList
 contourTree = undefined
 
 dim1  = (3,3,3)
-vert1 = reverse [1..9]
+vert1 = [1..9]
 ds1   = dataset dim1 (neighbourhood dim1) vert1
-tree1 = joinTree vert1 (mesh ds1)
+tree1 = joinTree [0..length vert1 -1] (mesh ds1)
 
 dim2  = (5,5,5)
-vert2 = reverse [1..5^3]
+vert2 = [1..5^3]
 ds2   = dataset dim2 (neighbourhood dim2) vert2
-tree2 = joinTree vert2 (mesh ds2)
+tree2 = joinTree [0..length vert2-1] (mesh ds2)
+
+dim3  = (2,2,1)
+vert3 = [1..4]
+ds3   = dataset dim3 (neighbourhood dim3) vert3
+tree3 = joinTree [0..length vert3 - 1] (mesh ds3)
+
+dim4  = (2,1,1)
+vert4 = [1,2]
+ds4   = dataset dim4 (neighbourhood dim4) vert4
+tree4 = joinTree [0..length vert4 - 1] (mesh ds4)
